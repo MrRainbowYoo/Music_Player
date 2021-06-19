b40bbfdc6480ab4f35e6cd4b6f24bab965818361
MrRainbowYoo <813806051@qq.com>
2021-06-19 21:43:46 +0800

    新增顶部栏前进后退功能

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="header">
         <h2>小陈音乐</h2>
+        <div class="btns">
+            <i class="el-icon-arrow-left" @click="go(-1)"></i>
+            <i class="el-icon-arrow-right" @click="go(1)"></i>            
+        </div>
         <div class="input-box">
             <el-input
             placeholder="请输入内容"
@@ -41,6 +45,9 @@ export default {
             var regu = "^[ ]+$";
             var re = new RegExp(regu);
             return re.test(str);
+        },
+        go(n) {
+            this.$router.go(n)
         }                
     }
 }
@@ -57,7 +64,23 @@ export default {
     .header h2 {
         color: #fff;
         letter-spacing: .5rem;
+        margin-right: 30px;
+    }
+
+    .btns {
         margin-right: auto;
+    }
+
+    .btns i {
+        color: #fff;
+        border-radius: 50%;
+        padding: 10px;
+        box-sizing: border-box;
+        cursor: pointer;
+    }
+
+    .btns i:hover {
+        background-color: rgba(255, 255, 255,.3);
     }
 
     .header .input-box {
